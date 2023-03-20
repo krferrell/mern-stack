0b9a398bccd9948940c46def66ae26316a13cf2f
Kenneth Ferrell <kferrell@alphaworks.tech>
2023-03-20 10:38:27 -0700

    feat(workoutApi): add tag types

--- frontend/src/api/workoutApi/workoutApi.ts
+++ frontend/src/api/workoutApi/workoutApi.ts
@@ -1,18 +1,36 @@
 // Need to use the React-specific entry point to import createApi
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { IWorkoutRequest } from './types';
+import { IWorkoutValues } from '../../components/workoutForm';
 
 // Define a service using a base URL and expected endpoints
-export const workoutApi = createApi({
+export const workoutApi: any = createApi({
   reducerPath: 'workoutApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:4000/' }),
+  tagTypes: ['Workouts'],
   endpoints: (builder) => ({
     getAllWorkouts: builder.query<any, string>({
       query: () => `api/workouts/`,
+      providesTags: ['Workouts'],
+    }),
+    deleteWorkout: builder.mutation<any, IWorkoutValues>({
+      query: (id) => ({ url: `api/workouts/${id}`, method: 'DELETE' }),
+      invalidatesTags: ['Workouts'],
+    }),
+    createWorkout: builder.mutation<any, IWorkoutValues>({
+      query: (payload) => ({
+        url: 'api/workouts',
+        method: 'POST',
+        body: payload,
+      }),
+      invalidatesTags: ['Workouts'],
     }),
   }),
 });
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetAllWorkoutsQuery } = workoutApi;
+export const {
+  useGetAllWorkoutsQuery,
+  useCreateWorkoutMutation,
+  useDeleteWorkoutMutation,
+} = workoutApi;
