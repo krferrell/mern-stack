a8c16c02eef650d0151ba36cc6134d6062b7cb15
Kenneth Ferrell <kferrell@alphaworks.tech>
2023-03-20 10:41:43 -0700

    feat(workoutList): temp update

--- frontend/src/components/workoutList/index.tsx
+++ frontend/src/components/workoutList/index.tsx
@@ -1,19 +1,26 @@
-import { FC } from 'react';
-import { useGetAllWorkoutsQuery } from '../../api/workoutApi/workoutApi';
+import { FC, useEffect, useState } from 'react';
+import {
+  useGetAllWorkoutsQuery,
+  useDeleteWorkoutMutation,
+} from '../../api/workoutApi/workoutApi';
+import { IWorkoutValues } from '../workoutForm';
 import { StyledWorkoutList } from './styled';
 import { IWorkout, IWorkoutListProps } from './types';
 
 const WorkoutList: FC<IWorkoutListProps> = () => {
-  const { data, isLoading, isError, error } = useGetAllWorkoutsQuery('');
+  const [deleteWorkout, result] = useDeleteWorkoutMutation('');
+  const { data, isLoading, isError, error } = useGetAllWorkoutsQuery('', {
+    refetchOnMountOrArgChange: true,
+  });
 
   const renderWorkouts = () => {
-    console.log(data);
-    return data?.map((workout: IWorkout) => {
+    return data?.map((workout: IWorkout, i: number) => {
       return (
-        <div key={workout.reps} style={{ color: 'white' }}>
+        <div key={i} style={{ color: 'black' }}>
+          <button onClick={() => deleteWorkout(workout._id)}>Delete</button>
           {workout.title}
           {workout.weight}
-          {workout.reps}
+          {workout.date}
         </div>
       );
     });
