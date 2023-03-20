33cd143af939c676e097e1d63b5f14602188004f
Kenneth Ferrell <kferrell@alphaworks.tech>
2023-03-20 10:39:00 -0700

    feat(HomePage): initialize home page

---
+++ frontend/src/components/homePage/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StyledHomePage = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  background: rgba(51, 51, 51, 0.7);
+  z-index: 0;
+`;
