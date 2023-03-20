4688492063304eb5c494e6e42c2dd5ac6844a8ff
Kenneth Ferrell <kferrell@alphaworks.tech>
2023-03-20 10:37:56 -0700

    format code

--- backend/server.js
+++ backend/server.js
@@ -4,26 +4,27 @@ const mongoose = require('mongoose');
 const express = require('express');
 const workoutRoutes = require('./routes/workouts');
 
-
 const app = express();
 
-// Middleware: runs on each request. Next() runs the actual requst.
+// Middleware: runs on each request. Next() runs the actual request.
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next();
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 //routes
 app.use('/api/workouts', workoutRoutes);
 
-//Connects to the database before the server allows for requests. 
-mongoose.connect(process.env.MONGO_URI).then(() => {
+//Connects to the database before the server allows for requests.
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
     app.listen(process.env.PORT, () => {
-        console.log('listening on port 4000')
-    })
-}).catch((err) => console.log(err))
-
-
+      console.log('listening on port 4000');
+    });
+  })
+  .catch((err) => console.log(err));
